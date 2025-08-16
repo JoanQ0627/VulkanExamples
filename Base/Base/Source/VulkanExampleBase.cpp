cf9617c8b3166d899721e493ff008724546e73f4
JoanQ <7414706+joanq@user.noreply.gitee.com>
2025-08-16 12:52:35 +0800

    test3

--- Base/Base/Source/VulkanExampleBase.cpp
+++ Base/Base/Source/VulkanExampleBase.cpp
@@ -9,7 +9,7 @@
 
 int main() {
     glfwInit();
-    // test
+    // test // test
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);

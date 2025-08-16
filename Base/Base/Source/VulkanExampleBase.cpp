cb8713ba80a0811451063fe4c80cfc9e0b8094eb
JoanQ <7414706+joanq@user.noreply.gitee.com>
2025-08-16 12:18:29 +0800

    TEST

--- Base/Base/Source/VulkanExampleBase.cpp
+++ Base/Base/Source/VulkanExampleBase.cpp
@@ -17,6 +17,8 @@ int main() {
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
     std::cout << extensionCount << " extensions supported\n";
+    std::cout << extensionCount << " extensions supported\n";
+
 
     glm::mat4 matrix;
     glm::vec4 vec;
